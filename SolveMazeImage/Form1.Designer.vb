6b808a26490592bbfe742048c614f83a85c95e23
縦椅子鉋 <TateIsuKanna@users.noreply.github.com>
2016-03-26 01:04:55 +0900

    追加

---
+++ SolveMazeImage/Form1.Designer.vb
@@ -0,0 +1,108 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+	Inherits System.Windows.Forms.Form
+
+    'フォームがコンポーネントの一覧をクリーンアップするために dispose をオーバーライドします。
+    <System.Diagnostics.DebuggerNonUserCode()>
+	Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+		Try
+			If disposing AndAlso components IsNot Nothing Then
+				components.Dispose()
+			End If
+		Finally
+			MyBase.Dispose(disposing)
+		End Try
+	End Sub
+
+    'Windows フォーム デザイナーで必要です。
+    Private components As System.ComponentModel.IContainer
+
+    'メモ: 以下のプロシージャは Windows フォーム デザイナーで必要です。
+    'Windows フォーム デザイナーを使用して変更できます。  
+    'コード エディターを使って変更しないでください。
+    <System.Diagnostics.DebuggerStepThrough()>
+	Private Sub InitializeComponent()
+		Me.OpenFileDialog1 = New System.Windows.Forms.OpenFileDialog()
+		Me.MenuStrip1 = New System.Windows.Forms.MenuStrip()
+		Me.ファイルFToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+		Me.ファイルを開くOToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+		Me.SaveFileDialog1 = New System.Windows.Forms.SaveFileDialog()
+		Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+		Me.Label1 = New System.Windows.Forms.Label()
+		Me.MenuStrip1.SuspendLayout()
+		CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+		Me.SuspendLayout()
+		'
+		'MenuStrip1
+		'
+		Me.MenuStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.ファイルFToolStripMenuItem})
+		Me.MenuStrip1.Location = New System.Drawing.Point(0, 0)
+		Me.MenuStrip1.Name = "MenuStrip1"
+		Me.MenuStrip1.RenderMode = System.Windows.Forms.ToolStripRenderMode.System
+		Me.MenuStrip1.Size = New System.Drawing.Size(713, 24)
+		Me.MenuStrip1.TabIndex = 1
+		Me.MenuStrip1.Text = "MenuStrip1"
+		'
+		'ファイルFToolStripMenuItem
+		'
+		Me.ファイルFToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.ファイルを開くOToolStripMenuItem})
+		Me.ファイルFToolStripMenuItem.Name = "ファイルFToolStripMenuItem"
+		Me.ファイルFToolStripMenuItem.Size = New System.Drawing.Size(66, 20)
+		Me.ファイルFToolStripMenuItem.Text = "ファイル(&F)"
+		'
+		'ファイルを開くOToolStripMenuItem
+		'
+		Me.ファイルを開くOToolStripMenuItem.Name = "ファイルを開くOToolStripMenuItem"
+		Me.ファイルを開くOToolStripMenuItem.ShortcutKeys = CType((System.Windows.Forms.Keys.Control Or System.Windows.Forms.Keys.O), System.Windows.Forms.Keys)
+		Me.ファイルを開くOToolStripMenuItem.Size = New System.Drawing.Size(185, 22)
+		Me.ファイルを開くOToolStripMenuItem.Text = "ファイルを開く(&O)"
+		'
+		'SaveFileDialog1
+		'
+		Me.SaveFileDialog1.DefaultExt = "png"
+		'
+		'PictureBox1
+		'
+		Me.PictureBox1.Dock = System.Windows.Forms.DockStyle.Fill
+		Me.PictureBox1.Location = New System.Drawing.Point(0, 24)
+		Me.PictureBox1.Name = "PictureBox1"
+		Me.PictureBox1.Size = New System.Drawing.Size(713, 447)
+		Me.PictureBox1.TabIndex = 2
+		Me.PictureBox1.TabStop = False
+		'
+		'Label1
+		'
+		Me.Label1.Anchor = CType((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+		Me.Label1.AutoSize = True
+		Me.Label1.Location = New System.Drawing.Point(453, 24)
+		Me.Label1.Name = "Label1"
+		Me.Label1.Size = New System.Drawing.Size(260, 12)
+		Me.Label1.TabIndex = 3
+		Me.Label1.Text = "スタート指定:クリック,ゴール指定:Ctrlを押しながらクリック"
+		'
+		'Form1
+		'
+		Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 12.0!)
+		Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+		Me.ClientSize = New System.Drawing.Size(713, 471)
+		Me.Controls.Add(Me.Label1)
+		Me.Controls.Add(Me.PictureBox1)
+		Me.Controls.Add(Me.MenuStrip1)
+		Me.MainMenuStrip = Me.MenuStrip1
+		Me.Name = "Form1"
+		Me.Text = "SolveMazeImage自称便利な迷路解き"
+		Me.MenuStrip1.ResumeLayout(False)
+		Me.MenuStrip1.PerformLayout()
+		CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+		Me.ResumeLayout(False)
+		Me.PerformLayout()
+
+	End Sub
+	Friend WithEvents OpenFileDialog1 As OpenFileDialog
+	Friend WithEvents MenuStrip1 As MenuStrip
+	Friend WithEvents ファイルFToolStripMenuItem As ToolStripMenuItem
+	Friend WithEvents ファイルを開くOToolStripMenuItem As ToolStripMenuItem
+	Friend WithEvents SaveFileDialog1 As SaveFileDialog
+	Friend WithEvents PictureBox1 As PictureBox
+	Friend WithEvents Label1 As Label
+End Class
---
+++ SolveMazeImage/Form1.vb
@@ -0,0 +1,114 @@
+﻿Public Class Form1
+	Private Structure visitinfo
+		Dim visited As Boolean
+		Dim from As Point
+	End Structure
+
+	Dim img As Bitmap
+
+	Private Sub ファイルを開くOToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles ファイルを開くOToolStripMenuItem.Click
+		If OpenFileDialog1.ShowDialog() = DialogResult.OK Then
+			img = New Bitmap(OpenFileDialog1.FileName)
+			PictureBox1.Image = img
+		End If
+	End Sub
+
+	Dim start As Point
+	Dim goal As Point
+
+	Private Sub PictureBox1_MouseDown(sender As Object, e As MouseEventArgs) Handles PictureBox1.MouseDown
+		If (Control.ModifierKeys And Keys.Control) = Keys.Control Then
+			goal = New Point(e.X, e.Y)
+
+			Dim map(img.Height - 1, img.Width - 1) As Boolean
+			Dim visited(img.Height - 1, img.Width - 1) As visitinfo
+
+			For y = 0 To img.Height - 1
+				For x = 0 To img.Width - 1
+					If img.GetPixel(x, y).R < 128 Then
+						map(y, x) = True
+					Else
+						map(y, x) = False
+					End If
+				Next
+			Next
+			solve_maze(map, visited)
+		Else
+			start = New Point(e.X, e.Y)
+		End If
+	End Sub
+
+	'TODO:無効なfromで判定するとvisited不要?
+
+	Private Sub solve_maze(ByRef map(,) As Boolean, ByRef visited(,) As visitinfo)
+		visited(start.Y, start.X).from = start
+		visited(start.Y, start.X).visited = True
+
+		Dim tansaku As New Queue(Of Point)
+		tansaku.Enqueue(start)
+
+		Do While tansaku.Count > 0
+			Dim nex As Point = tansaku.Dequeue()
+			PictureBox1.CreateGraphics.FillRectangle(Brushes.Blue, nex.X, nex.Y, 1, 1)
+			If nex.X = goal.X AndAlso nex.Y = goal.Y Then
+				Exit Do
+			End If
+			'HACK:Try無双やめよう
+			Try
+				If Not visited(nex.Y, nex.X + 1).visited Then
+					If Not map(nex.Y, nex.X + 1) Then
+						visited(nex.Y, nex.X + 1).from = nex
+						visited(nex.Y, nex.X + 1).visited = True
+						tansaku.Enqueue(New Point(nex.X + 1, nex.Y))
+					End If
+				End If
+			Catch ex As Exception
+			End Try
+			Try
+				If Not visited(nex.Y + 1, nex.X).visited Then
+					If Not map(nex.Y + 1, nex.X) Then
+						visited(nex.Y + 1, nex.X).from = nex
+						visited(nex.Y + 1, nex.X).visited = True
+						tansaku.Enqueue(New Point(nex.X, nex.Y + 1))
+					End If
+				End If
+			Catch ex As Exception
+			End Try
+			Try
+				If Not visited(nex.Y, nex.X - 1).visited Then
+					If Not map(nex.Y, nex.X - 1) Then
+						visited(nex.Y, nex.X - 1).from = nex
+						visited(nex.Y, nex.X - 1).visited = True
+						tansaku.Enqueue(New Point(nex.X - 1, nex.Y))
+					End If
+				End If
+			Catch ex As Exception
+			End Try
+			Try
+				If Not visited(nex.Y - 1, nex.X).visited Then
+					If Not map(nex.Y - 1, nex.X) Then
+						visited(nex.Y - 1, nex.X).from = nex
+						visited(nex.Y - 1, nex.X).visited = True
+						tansaku.Enqueue(New Point(nex.X, nex.Y - 1))
+					End If
+				End If
+			Catch ex As Exception
+			End Try
+		Loop
+
+		PictureBox1.Image = img
+		Dim g As Graphics = Graphics.FromImage(img)
+		Dim x As Integer = goal.X
+		Dim y As Integer = goal.Y
+		Do
+			g.FillRectangle(Brushes.Red, visited(y, x).from.X, visited(y, x).from.Y, 1, 1)
+			x = visited(y, x).from.X
+			y = visited(y, x).from.Y
+		Loop Until x = start.X AndAlso y = start.Y
+
+
+		If SaveFileDialog1.ShowDialog() = DialogResult.OK Then
+			img.Save(SaveFileDialog1.FileName, Imaging.ImageFormat.Png)
+		End If
+	End Sub
+End Class
